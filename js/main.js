a6d9e1b6b10cf3bfb5003db16279eec0de60c4d1
filippo199600 <pippotartaglia@gmail.com>
2023-08-31 16:17:43 +0200

    add list of object

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
+// creo la mia lista di oggetti
+const list = [
+  {
+    text: "fare la spesa",
+    done: false,
+  },
+  {
+    text: "portare a spasso il cane",
+    done: false,
+  },
+  {
+    text: "fare allenamento",
+    done: false,
+  },
+];
+
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      list,
     };
   },
 }).mount("#app");
